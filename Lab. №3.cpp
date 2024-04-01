b27f90454d9ccc30de50b5d8541f04218f2e0e85
i888844 <i888844@yandex.ru>
2024-04-01 10:45:43 +0300

    3rd commit

--- Lab. №3.cpp
+++ Lab. №3.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 #include <windows.h>
 
-void Eitkin_by_X(double function[2][11], double X)
+double Eitkin_by_X(double function[2][11], double X)
 {
     int i = 0;
     int j = 0;
@@ -99,6 +99,8 @@ void Eitkin_by_X(double function[2][11], double X)
 
     printf("Значение функции по интерполяционной формуле Лагранжа: %.2f\n", L);
     printf("\n\n\n");
+
+    return L;
 }
 
 int main()
@@ -131,45 +133,63 @@ int main()
     }
     printf("\n");
 
-    double X = 0.0;
+    double X[10];
+    double Xf[10];
+    double Ls[10];
 
-    X = function[0][2] + 0.25 * h;
-    printf("X = X0 + 0.25h = %.2f\n", X);
-    Eitkin_by_X(function, X);
+    X[0] = function[0][2] + 0.25 * h;
+    printf("\nX = X0 + 0.25h = %.2f\n", X[0]);
+    Ls[0] = Eitkin_by_X(function, X[0]);
+    Xf[0] = atan(1.0 / 2.0 * X[0]);
 
-    X = function[0][2] - 0.25 * h;
-    printf("X = X0 - 0.25h = %.2f\n", X);
-    Eitkin_by_X(function, X);
+    X[1] = function[0][2] - 0.25 * h;
+    printf("X = X0 - 0.25h = %.2f\n", X[1]);
+    Ls[1] = Eitkin_by_X(function, X[1]);
+    Xf[1] = atan(1.0 / 2.0 * X[1]);
 
-    X = function[0][2] + 0.5 * h;
-    printf("X = X0 + 0.5h = %.2f\n", X);
-    Eitkin_by_X(function, X);
+    X[2] = function[0][2] + 0.5 * h;
+    printf("X = X0 + 0.5h = %.2f\n", X[2]);
+    Ls[2] = Eitkin_by_X(function, X[2]);
+    Xf[2] = atan(1.0 / 2.0 * X[2]);
 
-    X = function[0][2] - 0.5 * h;
-    printf("X = X0 - 0.5h = %.2f\n", X);
-    Eitkin_by_X(function, X);
+    X[3] = function[0][2] - 0.5 * h;
+    printf("X = X0 - 0.5h = %.2f\n", X[3]);
+    Ls[3] = Eitkin_by_X(function, X[3]);
+    Xf[3] = atan(1.0 / 2.0 * X[3]);
 
-    X = function[0][5] + 0.5 * h;
-    printf("X = X5 + 0.5h = %.2f\n", X);
-    Eitkin_by_X(function, X);
+    X[4] = function[0][5] + 0.5 * h;
+    printf("X = X5 + 0.5h = %.2f\n", X[4]);
+    Ls[4] = Eitkin_by_X(function, X[4]);
+    Xf[4] = atan(1.0 / 2.0 * X[4]);
 
-    X = function[0][5] - 0.5 * h;
-    printf("X = X5 - 0.5h = %.2f\n", X);
-    Eitkin_by_X(function, X);
+    X[5] = function[0][5] - 0.5 * h;
+    printf("X = X5 - 0.5h = %.2f\n", X[5]);
+    Ls[5] = Eitkin_by_X(function, X[5]);
+    Xf[5] = atan(1.0 / 2.0 * X[5]);
 
-    X = function[0][7] + 0.25 * h;
-    printf("X = X7 + 0.25h = %.2f\n", X);
-    Eitkin_by_X(function, X);
+    X[6] = function[0][7] + 0.25 * h;
+    printf("X = X7 + 0.25h = %.2f\n", X[6]);
+    Ls[6] = Eitkin_by_X(function, X[6]);
+    Xf[6] = atan(1.0 / 2.0 * X[6]);
 
-    X = function[0][7] - 0.25 * h;
-    printf("X = X7 - 0.25h = %.2f\n", X);
-    Eitkin_by_X(function, X);
+    X[7] = function[0][7] - 0.25 * h;
+    printf("X = X7 - 0.25h = %.2f\n", X[7]);
+    Ls[7] = Eitkin_by_X(function, X[7]);
+    Xf[7] = atan(1.0 / 2.0 * X[7]);
 
-    X = function[0][10] + 1.5 * h;
-    printf("X = X10 + 1.5h = %.2f\n", X);
-    Eitkin_by_X(function, X);
+    X[8] = function[0][10] + 1.5 * h;
+    printf("X = X10 + 1.5h = %.2f\n", X[8]);
+    Ls[8] = Eitkin_by_X(function, X[8]);
+    Xf[8] = atan(1.0 / 2.0 * X[8]);
 
-    X = function[0][10] - 1.5 * h;
-    printf("X = X10 - 1.5h = %.2f\n", X);
-    Eitkin_by_X(function, X);
+    X[9] = function[0][10] - 1.5 * h;
+    printf("X = X10 - 1.5h = %.2f\n", X[9]);
+    Ls[9] = Eitkin_by_X(function, X[9]);
+    Xf[9] = atan(1.0 / 2.0 * X[9]);
+
+    printf("\n\nX      F(x) истин.   F(x) Лагранж   Погрешность\n");
+    for (i = 0; i < 10; i++)
+    {
+        printf("%.2f   %.8f    %.8f     %.8f\n", X[i], Ls[i], Xf[i], fabs(Xf[i] - Ls[i]));
+    }
 }
